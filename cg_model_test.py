bfc72135e95031faf30189405094883cbb634846
andrewtarzia <andrew.tarzia@gmail.com>
2023-07-29 11:18:25 +0200

    Finish script.

--- cg_model_test.py
+++ cg_model_test.py
@@ -20,6 +20,8 @@ import itertools
 import os
 from openmm import openmm
 from rdkit import RDLogger
+import matplotlib.pyplot as plt
+from matplotlib.lines import Line2D
 
 from cgexplore.ensembles import Ensemble
 from cgexplore.generation_utilities import (
@@ -341,14 +343,19 @@ def compare_final_energies(path1, path2):
     if ".out" in str(path1):
         e1 = get_final_energy(path1)
         e2 = get_final_energy(path2)
-        print(path1.name, path2.name, e1, e2)
+        # print(path1.name, path2.name, e1, e2)
         # assert np.isclose(e1, e2, atol=1e-2, rtol=0)
+        return e1, e2
     elif ".json" in str(path1):
         e1, id1 = get_final_energy(path1)
         e2, id2 = get_final_energy(path2)
-        print(path1.name, path2.name, e1, e2, id1, id2)
-        # assert np.isclose(e1, e2, atol=1e-2, rtol=0)
+        # print(path1.name, path2.name, e1, e2, id1, id2)
+        try:
+            assert np.isclose(e1, e2, atol=1e-1, rtol=0)
+        except AssertionError:
+            assert e1 > 5 and e2 > 5
         # assert id1 == id2
+        return e1, e2
 
 
 def main():
@@ -368,7 +375,6 @@ def main():
 
     struct_done = pathlib.Path().absolute() / "structures_done"
     calculation_done = pathlib.Path().absolute() / "calculations_done"
-    ligand_done = pathlib.Path().absolute() / "ligands_done"
 
     # Define bead libraries.
     beads_core_2c_lib = core_2c_beads()
@@ -478,13 +484,29 @@ def main():
                     )
                 cages.append(name)
 
+    fig, axs = plt.subplots(ncols=3, nrows=2, figsize=(16, 8))
+
+    ax = axs[0][0]
+    ax_bond = axs[0][1]
+    ax_angle = axs[0][2]
+    ax_tors = axs[1][0]
+    ax_rg = axs[1][1]
+    ax_md = axs[1][2]
+
     for i in cages:
+        if "ton" in i:
+            c = "r"
+        elif "toff" in i:
+            c = "gray"
+
         if "a00" in i:
             old = i.replace("a00", "a07")
         elif "a01" in i:
             old = i.replace("a01", "a014")
         elif "a02" in i:
             old = i.replace("a02", "a017")
+        elif "a03" in i:
+            old = i.replace("a03", "a018")
 
         if "n00" in i:
             old = old.replace("n00", "n02")
@@ -492,50 +514,191 @@ def main():
             old = old.replace("n01", "n04")
         elif "n02" in i:
             old = old.replace("n02", "n07")
+        elif "n03" in i:
+            old = old.replace("n03", "n01")
         # compare_final_energies(
         #     path1=calculation_done / f"{old}_opt1_omm.out",
         #     path2=calculation_output / f"{i}_opt1_omm.out",
         # )
-        compare_final_energies(
+        e1, e2 = compare_final_energies(
             path1=calculation_done / f"{old}_ensemble.json",
             path2=calculation_output / f"{i}_ensemble.json",
         )
-    raise SystemExit()
 
-    # for i in c2_blocks:
-    #     old = i.replace("a00", "a07").replace("a01", "a018")
-    #     compare_final_energies(
-    #         path1=calculation_done / f"{old}_omm.out",
-    #         path2=calculation_output / f"{i}_omm.out",
-    #     )
+        ax.scatter(
+            e1,
+            e2,
+            c=c,
+            edgecolor="none",
+            s=50,
+            alpha=1.0,
+        )
 
-    # for i in c3_blocks:
-    #     print(i)
-    #     old = i.replace("n01", "n07").replace("n00", "n01")
-    #     print(old)
-    #     compare_final_energies(
-    #         path1=calculation_done / f"{old}_omm.out",
-    #         path2=calculation_output / f"{i}_omm.out",
-    #     )
-    # raise SystemExit()
+        custom_torsion_atoms = ("Pb", "Ba", "Ag", "Ba", "Pb")
 
-    # g_measure = GeomMeasure(custom_torsion_atoms)
-    # bond_data = g_measure.calculate_bonds(conformer.molecule)
-    # angle_data = g_measure.calculate_angles(conformer.molecule)
-    # dihedral_data = g_measure.calculate_torsions(
-    #     molecule=conformer.molecule,
-    #     absolute=True,
-    #     path_length=5,
-    # )
-    # min_b2b_distance = g_measure.calculate_minb2b(conformer.molecule)
-    # radius_gyration = g_measure.calculate_radius_gyration(
-    #     molecule=conformer.molecule,
-    # )
-    # max_diameter = g_measure.calculate_max_diameter(conformer.molecule)
-    # if radius_gyration > max_diameter:
-    #     raise ValueError(
-    #         f"{name} Rg ({radius_gyration}) > maxD ({max_diameter})"
-    #     )
+        new_struct = stk.BuildingBlock.init_from_file(
+            str(struct_output / f"{i}_optc.mol")
+        )
+        old_struct = stk.BuildingBlock.init_from_file(
+            str(struct_done / f"{old}_optc.mol")
+        )
+
+        assert new_struct.get_num_atoms() == old_struct.get_num_atoms()
+        assert new_struct.get_num_bonds() == old_struct.get_num_bonds()
+
+        g_measure = GeomMeasure(custom_torsion_atoms)
+        bond_data1 = g_measure.calculate_bonds(old_struct)
+        bond_data2 = g_measure.calculate_bonds(new_struct)
+        for i in bond_data1:
+            assert i in bond_data2
+            assert len(bond_data1[i]) == len(bond_data2[i])
+            for bd1, bd2 in zip(bond_data1[i], bond_data2[i]):
+                # print(bd1, bd2)
+                # assert np.isclose(bd1, bd2, atol=1e-1, rtol=0)
+                ax_bond.scatter(
+                    bd1,
+                    bd2,
+                    c=c,
+                    edgecolor="none",
+                    s=50,
+                    alpha=1.0,
+                )
+
+        angle_data1 = g_measure.calculate_angles(old_struct)
+        angle_data2 = g_measure.calculate_angles(new_struct)
+        for i in angle_data1:
+            assert i in angle_data2
+            assert len(angle_data1[i]) == len(angle_data2[i])
+            for bd1, bd2 in zip(angle_data1[i], angle_data2[i]):
+                # print(bd1, bd2)
+                # assert np.isclose(bd1, bd2, atol=1, rtol=0)
+                ax_angle.scatter(
+                    bd1,
+                    bd2,
+                    c=c,
+                    edgecolor="none",
+                    s=50,
+                    alpha=1.0,
+                )
+
+        dihedral_data1 = g_measure.calculate_torsions(
+            molecule=old_struct,
+            absolute=True,
+            path_length=5,
+        )
+        dihedral_data2 = g_measure.calculate_torsions(
+            molecule=new_struct,
+            absolute=True,
+            path_length=5,
+        )
+        for i in dihedral_data1:
+            assert i in dihedral_data2
+            assert len(dihedral_data1[i]) == len(dihedral_data2[i])
+            for bd1, bd2 in zip(dihedral_data1[i], dihedral_data2[i]):
+                # assert np.isclose(bd1, bd2, atol=1, rtol=0)
+                ax_tors.scatter(
+                    bd1,
+                    bd2,
+                    c=c,
+                    edgecolor="none",
+                    s=50,
+                    alpha=1.0,
+                )
+
+        max_diameter1 = g_measure.calculate_max_diameter(old_struct)
+        max_diameter2 = g_measure.calculate_max_diameter(new_struct)
+        # print(max_diameter1, max_diameter2)
+        # assert np.isclose(bd1, bd2, atol=1, rtol=0)
+        ax_md.scatter(
+            max_diameter1,
+            max_diameter2,
+            c=c,
+            edgecolor="none",
+            s=50,
+            alpha=1.0,
+        )
+
+        radius_gyration1 = g_measure.calculate_radius_gyration(old_struct)
+        radius_gyration2 = g_measure.calculate_radius_gyration(new_struct)
+        # print(radius_gyration1, radius_gyration2)
+        # assert np.isclose(bd1, bd2, atol=1, rtol=0)
+        ax_rg.scatter(
+            radius_gyration1,
+            radius_gyration2,
+            c=c,
+            edgecolor="none",
+            s=50,
+            alpha=1.0,
+        )
+
+    ax.tick_params(axis="both", which="major", labelsize=16)
+    ax.set_xlabel("first run energy", fontsize=16)
+    ax.set_ylabel("second run energy", fontsize=16)
+    ax.set_xlim(0.001, 100)
+    ax.set_ylim(0.001, 100)
+    ax.plot([0, 100], [0, 100], c="k", ls="--")
+    ax.set_xscale("log")
+    ax.set_yscale("log")
+
+    ax_bond.tick_params(axis="both", which="major", labelsize=16)
+    ax_bond.set_xlabel("first run bonds", fontsize=16)
+    ax_bond.set_ylabel("second run bonds", fontsize=16)
+    ax_bond.set_xlim(0.9, 1.6)
+    ax_bond.set_ylim(0.9, 1.6)
+    ax_bond.plot([0.9, 1.6], [0.9, 1.6], c="k", ls="--")
+
+    ax_angle.tick_params(axis="both", which="major", labelsize=16)
+    ax_angle.set_xlabel("first run angles", fontsize=16)
+    ax_angle.set_ylabel("second run angles", fontsize=16)
+    ax_angle.set_xlim(0, 180)
+    ax_angle.set_ylim(0, 180)
+    ax_angle.plot([0, 180], [0, 180], c="k", ls="--")
+
+    ax_tors.tick_params(axis="both", which="major", labelsize=16)
+    ax_tors.set_xlabel("first run tors", fontsize=16)
+    ax_tors.set_ylabel("second run tors", fontsize=16)
+    ax_tors.set_xlim(0, 180)
+    ax_tors.set_ylim(0, 180)
+    ax_tors.plot([0, 180], [0, 180], c="k", ls="--")
+
+    ax_rg.tick_params(axis="both", which="major", labelsize=16)
+    ax_rg.set_xlabel("first run rgs", fontsize=16)
+    ax_rg.set_ylabel("second run rgs", fontsize=16)
+    ax_rg.set_xlim(0, 10)
+    ax_rg.set_ylim(0, 10)
+    ax_rg.plot([0, 10], [0, 10], c="k", ls="--")
+
+    ax_md.tick_params(axis="both", which="major", labelsize=16)
+    ax_md.set_xlabel("first run max diams", fontsize=16)
+    ax_md.set_ylabel("second run max diams", fontsize=16)
+    ax_md.set_xlim(5, 15)
+    ax_md.set_ylim(5, 15)
+    ax_md.plot([5, 15], [5, 15], c="k", ls="--")
+
+    legend_elements = []
+    for tstr in {"ton": "r", "toff": "gray"}:
+        legend_elements.append(
+            Line2D(
+                [0],
+                [0],
+                marker="o",
+                color="w",
+                label=tstr,
+                markerfacecolor={"ton": "r", "toff": "gray"}[tstr],
+                markersize=7,
+                markeredgecolor="none",
+                alpha=1.0,
+            )
+        )
+    ax.legend(handles=legend_elements, fontsize=16, ncol=1)
+
+    fig.tight_layout()
+    fig.savefig(
+        "parity.png",
+        dpi=360,
+        bbox_inches="tight",
+    )
+    plt.close()
 
     shutil.rmtree(calculation_output)
     shutil.rmtree(struct_output)
